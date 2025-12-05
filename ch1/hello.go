ed96a04e9bad718b276ba7acd95a1b00f635f133
Joaquin <perezmoralesJ30@gmail.com>
2025-12-05 18:48:05 +0100

    First Go Hello world!

---
+++ ch1/hello.go
@@ -0,0 +1,9 @@
+// A package clause starts every source file.
+// main is a special name declaring an executable rather than a library.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
